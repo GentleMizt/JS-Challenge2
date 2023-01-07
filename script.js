2595e834694b7f17a5eb2a3c5cb50f35979ecef0
GentleMizt <olamideiyanda18@gmail.com>
2023-01-07 18:42:15 +0100

    single string reverse done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-    const data = prompt('Input your desired statement');
+   
     // console.log(data);
-    let datas = data.split(' ');
+    // let datas = data.split('');
+    // console.log(datas);
+    // let dataReverse = datas.reverse();
+    // console.log(dataReverse);
+    // let joinReverse = dataReverse.join('');
+    // console.log(joinReverse);
+    // datas.split(' ').reverse().join('');
+
+    const data = prompt('Input your desired statement');
+    function reverseString() {
+        let datas = data.split('').reverse().join('');
+        alert(datas);
+    }
+    
+    reverseString();
+
+    
